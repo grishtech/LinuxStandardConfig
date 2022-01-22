b4eb3c1b6066999cff46a62f4f133a1ed1a3f033
grishtech <42551828+grishtech@users.noreply.github.com>
2022-01-22 13:21:00 -0800

    Disable local-storage

--- files/bashconfig.sh
+++ files/bashconfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 alias top='sudo bpytop'
 alias ports='sudo lsof -i | grep LISTEN'
-alias installk3s='curl -sfL https://get.k3s.io | sh -s - server --disable traefik --disable servicelb'
+alias installk3s='curl -sfL https://get.k3s.io | sh -s - server --disable traefik --disable servicelb --disable local-storage'
