921bf2171df1e14be41d0db7123ef2f196513e2c
grishtech <42551828+grishtech@users.noreply.github.com>
2022-06-19 16:45:58 -0700

    Update bashconfig.sh

--- files/bashconfig.sh
+++ files/bashconfig.sh
@@ -2,3 +2,4 @@
 alias top='sudo bpytop'
 alias ports='sudo lsof -i | grep LISTEN'
 alias installk3s='curl -sfL https://get.k3s.io | sh -s - server --disable traefik --disable servicelb --disable local-storage'
+alias ips='ip a'
